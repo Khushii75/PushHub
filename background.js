4632337e0fed8ba3cb8e31cc1abb23fa37873b5a
Khushi Kunwar <kunwarkhushi75@gmail.com>
2026-09-08 13:18:34 +0530

    Use production backend

--- background.js
+++ background.js
@@ -16,7 +16,7 @@ const GITHUB_REDIRECT_URI =
     "https://bhhpegeenjalkhlajmhekhngmcgjgaki.chromiumapp.org/";
 
 const BACKEND_URL =
-    "http://localhost:3000";
+    "https://pushhub-ol22.onrender.com";
 
 const README_START_MARKER =
     "<!-- DSA-GRINDHUB:START -->";
